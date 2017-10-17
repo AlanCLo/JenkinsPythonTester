db0d74dd9212cb69fc2bb48300a0d0144db0dfe2
Alan Lo <lo.alan@gmail.com>
2017-10-17 11:06:59 +1100

    made the site auth just in case i leave this somewhere

--- blahapp/urls.py
+++ blahapp/urls.py
@@ -1,11 +1,17 @@
 from django.conf.urls import url
 from . import views
 
+from django.contrib.auth import views as auth_views
+
 
 urlpatterns = [
-   url(r'^$', views.cat_list, name='cat_list'),
-   url(r'^cat/new/$', views.cat_new, name='cat_new'),
-   url(r'^cat/(?P<pk>\d+)/$', views.cat_detail, name='cat_detail'),
-   url(r'^cat/(?P<pk>\d+)/edit/$', views.cat_edit, name='cat_edit'),
-   url(r'^cat/(?P<pk>\d+)/delete/$', views.cat_delete, name='cat_delete'),
+	url(r'^$', views.cat_list, name='cat_list'),
+	url(r'^cat/new/$', views.cat_new, name='cat_new'),
+	url(r'^cat/(?P<pk>\d+)/$', views.cat_detail, name='cat_detail'),
+	url(r'^cat/(?P<pk>\d+)/edit/$', views.cat_edit, name='cat_edit'),
+	url(r'^cat/(?P<pk>\d+)/delete/$', views.cat_delete, name='cat_delete'),
+
+	url(r'^accounts/login/', auth_views.login, name='login'),
+	url(r'^accounts/logout/', auth_views.logout, {'next_page': '/'}, name='logout'),
+	url(r'^accounts/profile/', views.cat_list, name='profile'), # Cheat and just redirect to home	
 ]
--- blahapp/views.py
+++ blahapp/views.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, redirect, get_object_or_404
 from .models import Cat
 from .forms import CatForm
@@ -11,7 +12,7 @@ def cat_list(request):
 	cats = Cat.objects.all().order_by('created_date')
 	return render(request, 'blahapp/cat_list.html', { 'cats': cats })
 
-
+@login_required
 def cat_new(request):
 	if request.method == "POST":
 		form = CatForm(request.POST)
@@ -27,6 +28,7 @@ def cat_detail(request, pk):
 	cat = get_object_or_404(Cat, pk=pk)
 	return render(request, 'blahapp/cat_detail.html', {'cat': cat})
 
+@login_required
 def cat_edit(request, pk):
 	cat = get_object_or_404(Cat, pk=pk)
 	if request.method == "POST":
