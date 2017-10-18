f92070d67909118c1fc1a671db335533080b0973
Alan Lo <lo.alan@gmail.com>
2017-10-18 16:22:55 +1100

    just updating the cheatsheet

--- EchoDockerCheatSheet.sh
+++ EchoDockerCheatSheet.sh
@@ -5,6 +5,14 @@
 echo """
 Cheatsheet
 =====
+#Install docker on 14.04 ubuntu
+wget -qO- https://get.docker.com/ | sh
+
+#Install java 8 on 14.04 ubuntu
+sudo apt-add-repository ppa:webupd8team/java
+sudo apt-get update
+sudo apt-get install oracle-java8-installer
+=====
 docker run -d -it --expose 8000 -P --name=jt python:2.7.10 python
 
 docker ps
