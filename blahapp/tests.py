370a3401227874c60df5bee4fabe77dc97995c76
Alan Lo <lo.alan@gmail.com>
2017-10-18 13:35:03 +1100

    more tests

--- blahapp/tests.py
+++ blahapp/tests.py
@@ -3,6 +3,10 @@ from __future__ import unicode_literals
 
 from django.test import TestCase
 from blahapp.models import Human, Cat
+from django.utils import timezone
+from django.core.urlresolvers import reverse
+from blahapp.forms import CatForm
+
 
 # Create your tests here.
 
@@ -16,5 +20,22 @@ class ModelTestCase(TestCase):
 	def test_created(self):
 		self.assertEqual(len(Human.objects.all()), 1)
 		self.assertEqual(len(Cat.objects.all()), 2)
+
+	#def test_tostring_for_coverage(self):
+	#	self.assertEqual(str(Human.objects.all()[0]), "Human: Alan")
+	#	self.assertEqual(str(Cat.objects.all()[0]), "Cat: lion")
+
+
+class CatViewTestCase(TestCase):
+	def setUp(self):
+		h = Human.objects.create(name="Alan")
+		Cat.objects.create(name="lion", owner=h)
+
+	def test_cat_list_view(self):
+		url = reverse("cat_list")
+		resp = self.client.get(url)
+
+		self.assertEqual(resp.status_code, 200)
+		self.assertIn("lion", resp.content)
 
 
